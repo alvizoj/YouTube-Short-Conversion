e8be0b5bd5bb77048ee9357232510f4310792fbf
Joseph <alvizoj@uci.edu>
2023-06-30 15:36:13 -0700

    Deleted unused files

--- content.js
+++
@@ -1,7 +0,0 @@
-const currentURL = window?.location?.href ?? null;
-
-if (currentURL.startsWith('https://www.youtube.com/shorts')) {
-    const videoId = currentURL.split("/shorts/")[1];
-    const redirectURL = `https://www.youtube.com/watch?v=${videoId}`;
-    window.location.href = redirectURL;
-}
